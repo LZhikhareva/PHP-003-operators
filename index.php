98229382189b7a3e655ddb93077405886a5604df
Liza Zhikhareva <lisalisalisa57@mail.ru>
2025-03-30 18:10:54 +0300

    small changes

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
 
-$surname = $argv[1];
-$name = $argv[2];
-$lastname = $argv[3];
+echo "Введите фамилию: ";
+$surname = trim(fgets(STDIN));
+echo "Введите имя: ";
+$name = trim(fgets(STDIN));
+echo "Введите отчество: ";
+$lastname = trim(fgets(STDIN));
 
-// Формируем полное имя
-$fullName =  mb_convert_case("$surname $name $lastname", MB_CASE_TITLE, "UTF-8");
-echo "Полное имя: $fullName" . PHP_EOL;
+if ($surname == "") {
+    echo "Фамилия не указана";
+} else if ($name == "") {
+    echo "Имя не указано";
+} else if ($lastname == "") {
+    echo "Отчество не указано";
+} else {
+    // Формируем полное имя
+    $fullName =  mb_convert_case("$surname $name $lastname", MB_CASE_TITLE, "UTF-8");
+    echo "Полное имя: $fullName" . PHP_EOL;
 
-// Фамилия и инициалы
-$surnameAndInitials = mb_convert_case($surname, MB_CASE_TITLE, "UTF-8") . " " . mb_convert_case(mb_substr($name, 0, 1), MB_CASE_TITLE, "UTF-8") . "." . mb_convert_case(mb_substr($lastname, 0, 1), MB_CASE_TITLE, "UTF-8") . ".";
-echo "Фамилия и инициалы: $surnameAndInitials" . PHP_EOL;
+    // Фамилия и инициалы
+    $surnameAndInitials = mb_convert_case($surname, MB_CASE_TITLE, "UTF-8") . " " . mb_convert_case(mb_substr($name, 0, 1), MB_CASE_TITLE, "UTF-8") . "." . mb_convert_case(mb_substr($lastname, 0, 1), MB_CASE_TITLE, "UTF-8") . ".";
+    echo "Фамилия и инициалы: $surnameAndInitials" . PHP_EOL;
 
-// Аббревиатура
-$fio = mb_convert_case(mb_substr($surname, 0, 1), MB_CASE_TITLE, "UTF-8") . mb_convert_case(mb_substr($name, 0, 1), MB_CASE_TITLE, "UTF-8") . mb_convert_case(mb_substr($lastname, 0, 1), MB_CASE_TITLE, "UTF-8");
-echo "Аббревиатура: $fio" . PHP_EOL;
+    // Аббревиатура
+    $fio = mb_convert_case(mb_substr($surname, 0, 1), MB_CASE_TITLE, "UTF-8") . mb_convert_case(mb_substr($name, 0, 1), MB_CASE_TITLE, "UTF-8") . mb_convert_case(mb_substr($lastname, 0, 1), MB_CASE_TITLE, "UTF-8");
+    echo "Аббревиатура: $fio" . PHP_EOL;
+}
+
+
